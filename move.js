479739a0bfc15eb798a0bc4cae3f3871fec23781
splitube <voicesplit@daum.net>
2021-07-16 20:34:00 +0900

    character2 keep juming

--- move.js
+++ move.js
@@ -1,9 +1,15 @@
 const character = document.querySelector('.character').style;
+const character2 = document.querySelector('.character2').style;
+
 let ch_top = 472;
 let ch_left = 469;
 
 character.top = `${ch_top}px`;
 character.left =  `${ch_left}px`;
+
+character2.top = `${ch_top}px`;
+character2.left =  `${ch_left-300}px`;
+
 const TOP_MAX = 550;
 const TOP_MIN = 434;
 const LEFT_MAX = 865;
@@ -12,6 +18,8 @@ const LEFT_MIN = 169;
 const amount = 6;
 
 document.addEventListener('keydown', characterMove);
+
+character2.addEventListener()
 
 function characterMove(e){
 
